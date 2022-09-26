62587889839be00deccc1b7283d47abca4849e44
Lema25 <56377966+Lema25@users.noreply.github.com>
2022-09-26 18:58:36 -0500

    Add files via upload

---
+++ src/test/java/gestion/Zona.java
@@ -0,0 +1,56 @@
+package gestion;
+
+import java.util.ArrayList;
+
+import zooAnimales.Animal;
+import zooAnimales.Reptil;
+import zooAnimales.Ave;
+import zooAnimales.Pez;
+import zooAnimales.Mamifero;
+import zooAnimales.Anfibio;
+
+public class Zona {
+	
+	private String nombre;
+	private Zoologico zoo;
+	private ArrayList<Animal> animales = new ArrayList<Animal>();
+	
+	public Zona() {
+		super();
+	}
+
+	public Zona(String nombre, Zoologico zool) {
+		super();
+		this.nombre = nombre;
+		this.zoo = zool;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Zoologico getZoo() {
+		return zoo;
+	}
+	public void setZoo(Zoologico zoo) {
+		this.zoo = zoo;
+	}
+
+	public ArrayList<Animal> getAnimales() {
+		return animales;
+	}
+	public void setAnimales(ArrayList<Animal> animales) {
+		this.animales = animales;
+	}
+	
+	public void agregarAnimales(Animal nAnimal) {
+		animales.add(nAnimal);
+	}
+	
+	public int cantidadAnimales() {
+		return animales.size();
+	}
+}
---
+++ src/test/java/gestion/Zoologico.java
@@ -0,0 +1,52 @@
+package gestion;
+
+import java.util.ArrayList;
+
+public class Zoologico {
+	private String nombre;
+	private String ubicacion;
+	private ArrayList<Zona> zona = new ArrayList<Zona>();
+	
+	public Zoologico() {
+		super();
+	}
+
+	public Zoologico(String nombre, String ubicacion) {
+		super();
+		this.nombre = nombre;
+		this.ubicacion = ubicacion;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public ArrayList<Zona> getZona() {
+		return zona;
+	}
+	public void setZona(ArrayList<Zona> zonas) {
+		this.zona = zonas;
+	}
+	
+	public void agregarZonas(Zona nZona){
+		zona.add(nZona);
+	}
+	
+	public int cantidadTotalAnimales() {
+		int cont = 0;
+		for (int i = 0; i < zona.size(); i++)
+	        cont += zona.get(i).cantidadAnimales();
+		return cont;
+	}
+	
+}
